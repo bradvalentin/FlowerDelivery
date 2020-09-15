4531aace87ed44a4dd231306b00c6c837ae8ba9c
Vali-Home <valentin.brad@decode.ro>
2020-09-15 23:18:19 +0300

    create ui for splash screen

---
+++ app/src/main/java/com/example/flowerdelivery/SplashScreenActivity.kt
@@ -0,0 +1,31 @@
+package com.example.flowerdelivery
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+
+private const val SPLASH_DISPLAY_LENGTH = 2000
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val handler: Handler by lazy { Handler() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, SPLASH_DISPLAY_LENGTH.toLong())
+
+    }
+
+    override fun onStop() {
+        handler.removeCallbacksAndMessages(null)
+        finish()
+        super.onStop()
+    }
+}
